1a66852577ddcc49960c699f5b0d32347451816a
Colin Osidiana <osidianacolin@gmail.com>
2026-03-16 18:59:27 +0300

    rewrite from neuron level, gradient descent starts to work

--- Python_only/neuron.py
+++ Python_only/neuron.py
@@ -7,11 +7,50 @@
 import random as rand
 import math
 
+#activations 
+class ReLU:
+    def activate(self,input):
+        output=0
+        
+        if input > 0:
+            self.output=input
+        else: 
+            self.output=0
+        return self.output 
+    def derive(self,outputs):
+        derivative=0
+        
+        if output > 0:
+            derivative=1
+        else:
+            derivative=0
+        return derivative 
+
+class Linear:
+    def activate(self,input):
+        return input 
+    def derive(self,outputs):
+        return 1
+
+#losses
+class MSE:
+    def getloss(self,predicted, expected):
+        loss=(expected-predicted)**2
+        return loss
+    def derive(self,predicted, expected):
+        derivative=2*(predicted-expected)
+        return derivative 
+
+
+
 
 class neuron:
-    def __init__(self, num_inputs):
+    def __init__(self, num_inputs, actfn, lossfn, lr):
         
         # biases are initialised at 0 by convention cause weights are already randomised
+        self.actfn=actfn
+        self.lr=lr
+        self.lossfn=lossfn 
         self.bias=0
         self.weights=[]
         for i in range(num_inputs):
@@ -23,318 +62,73 @@ class neuron:
         for x, w in zip(inputs, self.weights):
             self.z+=x*w 
         self.z+=self.bias
+        self.z=self.actfn.activate(self.z)
+        return self.z 
+    def train(self, data):
+        # data is a list of the inputs, and expected outputs at index 0 and 1 respectively 
+        inputs=data[0]
+        prediction=self.forward(inputs)
+        expectation=data[1]
+        loss=self.lossfn.getloss(prediction, expectation)
+        print(loss)
+        # get derivatives
+        lossdv=self.lossfn.derive(prediction, expectation)
+        actdv=self.actfn.derive(prediction)
 
+        bdv=lossdv*actdv 
+        wdv=[]
 
-class layer:
-    def __init__(self, num_inputs, num):
-        self.neurons=[]
-        for i in range(num):
-            self.neurons.append(neuron(num_inputs))
-        self.weights=[]
-        self.biases=[]
-        for i in self.neurons:
-            self.weights.append(i.weights)
-            self.biases.append(i.bias)
-    def forward(self, inputs):
-        self.outputs=[]
-        for i in self.neurons:
-            i.forward(inputs)
-            self.outputs.append(i.z)
-    def update(self):
-        for n,w,b in zip(self.neurons, self.weights, self.biases):
-            print(n.weights, w,"\n")
-            print(n.bias, b,"\n")
-            n.weights=w 
-            n.bias=b
-            print(n.weights, w,"\n")
-            print(n.bias, b,"\n")
+        for x in inputs:
+            dv=bdv*x 
+            wdv.append(dv)
 
-            
-
-
-
-
-class ReLU:
-    def forward(self, inputs):
-        self.outputs=[]
-        for i in inputs:
-            if i > 0:
-                self.outputs.append(i)
-            else:
-                self.outputs.append(0)
-        return self.outputs
-    def derive(self, input):
-        self.derivatives=[]
-        for i in input:
-            if i > 0:
-                self.derivatives.append(1)
-            else:
-                self.derivatives.append(0)
-        return self.derivatives
-
-
-class MSE_loss:
-    def forward(self, predicted, expected):
-        self.loss=0
-        for ex,pre in zip(expected,predicted):
-            self.loss+=((ex-pre)**2)
-        return self.loss
-    def derive(self, predicted, expected):
-        self.gradient=0
-        for ex,pre in zip(expected,predicted):
-            # remember, the derivative is always y-hat - y. it determines the direction in which gradients move
-            self.gradient+=(2*(pre-ex))
-        self.gradient=self.gradient/len(predicted)
-        return self.gradient
-       
-
-
-
-# testing individual layers
-'''
-inputs=[1,2,3]
-layer1=layer(len(inputs), 4)
-layer1.forward(inputs)
-print(layer1.outputs)
-print(layer1.weights)
-print(layer1.biases)
-'''
-class net:
-    def __init__(self, num_inputs, layer_dims, actfn, lossfn, lr):
-        self.lossfn=lossfn
-        self.actfn=actfn
+        # now, learning occurs
+        self.bias-=bdv*self.lr
+        for i in range(len(self.weights)):
+            self.weights[i]-=wdv[i]*self.lr 
         
-        
-        
-        self.dimensions=layer_dims
-        self.layers=[]
-        for i in range(len(layer_dims)):
-
-            if i==0:
-                self.layers.append(layer(num_inputs, layer_dims[i]))
-            else:
-                self.layers.append(layer(layer_dims[i-1], layer_dims[i]))
-        self.weights=[]
-        self.biases=[]
-        self.lr=lr 
-
-
-        for i in self.layers:
-            self.weights.append(i.weights)
-            self.biases.append(i.biases)
-        
-        
-
-    def forward(self, inputs):
-        self.buff=inputs
-        self.results=[inputs]
-        
-        for weights, biases in zip(self.weights, self.biases):
-            out=[]
-            
-            for w,b in zip(weights,biases):
-                z=0
-                for m, x in zip(w, self.buff):
-                    z+=m*x 
-                z+=b 
-                out.append(z)
-            
-            
-            if weights!=self.weights[-1]:
-                out=self.actfn.forward(out)
-                self.results.append(out)
-
-            self.buff=out
-                        
-            
-        
-        self.outputs2=self.buff
-        return self.outputs2 
-
-    def optimisedtrain(self, dataset, velocity=False):
-
-        avgloss=0
-        avglossderiv=0
-        for i in dataset:
-            prediction=self.forward(i[0])
-            #print("Prediction",prediction)
-            expectation=i[1]
-            #print("Expectation",expectation)
-            avgloss+=self.lossfn.forward(prediction, expectation)
-            avglossderiv=self.lossfn.derive(prediction, expectation)
-        avgloss=avgloss/len(dataset)
-        avglossderiv=avglossderiv/len(dataset)
-
-        veloprovided=False
-        print(avgloss)
-        if velocity==False:
-            velocity=[0]*len(self.dimensions)
-            veloprovided=False
-        else:
-            velocity=velocity
-            veloprovided=True
-        # initialises derivative buffers as the loss derivative 
-        weightderivatives=avglossderiv
-        # this is just as the previous comment said, but technically not the right way to do it
-
-        prevadv=[1]*self.dimensions[-1]
-        for i in range(len(self.layers) -1, -1, -1):
-             
-            cweights=self.weights[i]
-            cbiases=self.biases[i]
-            coutputs=self.results[i]
-            
-            aderivative=[]
-            wderv=[]
-            bderv=[]
-            wdv=0
-            prevelo=velocity[i]
-            cvelo=[]
-            # checks if  current layer is last layer 
-            
-            if i == (len(self.layers)-1):
-                #if last layyer, all activation derivatives are set to 1, basically not affecting anything
-                # else the derivative is the respective derivative of the activation function
-                aderivative=[1]*len(coutputs)
-            else:
-                aderivative=self.actfn.derive(coutputs)
-            # generated derivatives for weights
-            for t in range(len(cweights)):
-                neuron_wderv=[]
-                for u in range(len(cweights[t])):
-                    x=coutputs[u]
-                    dv=weightderivatives*aderivative[u]*x 
-
-                    neuron_wderv.append(dv)
-                    wdv+=dv
-                wderv.append(neuron_wderv)
-                cvelo.append(neuron_wderv)
-
-            for n in range(len(cbiases)):
-                bderv.append(weightderivatives*prevadv[n])
-
-            for j in range(len(cweights)):
-                for k in range(len(cweights[j])):
-                    vlct=0
-                    lrsize=1
-                    if wderv[j][k]!=0:
-                        lrsize=1000/wderv[j][k]
-                    else:
-                        lrsize=1
-                    if veloprovided==True:
-                        vlct=prevelo[j][k]
-                    else:
-                        vlct=0
-                    #print("velo:",vlct)
-                    #print("Base lr*deriv:",self.lr*wderv[j][k])
-
-                    #print("cweight before:", cweights[j][k])
-                    cweights[j][k]-=(self.lr*lrsize*wderv[j][k]+vlct*0.9)
-                    #print("cweight after:", cweights[j][k])
-            for l in range(len(cbiases)):
-                cbiases[l]-=self.lr*bderv[l]
-            weightderivatives=wdv
-
-            self.weights[i]=cweights
-            self.biases[i]=cbiases
-            prevadv=aderivative
-            velocity[i]=cvelo
-        
-        newavgloss=0
-
-        for j in dataset:
-            prediction=self.forward(j[0])
-            #print("prediction", prediction)
-            expectation=j[1]
-            #print("expectation",expectation)
-            newavgloss+=self.lossfn.forward(prediction, expectation)
-        newavgloss=newavgloss/len(dataset)
-
-        print(newavgloss)
-
-
-        
-        return velocity
-    def optvartrain(self,dataset, batchsize, epochs):
-        velocity=[]
-        numbatches=math.floor(len(dataset)/batchsize)
-
-        changing=True
-        bchange=True 
-
-        for i in range(epochs):
-            velo=velocity 
-            print("Epoch: ", i)
-            for j in range(numbatches):
-                bdataset=dataset[(j*batchsize):((j+1)*batchsize)]
-                
-                if (i==0) & (j==0):
-                    velo=self.optimisedtrain(bdataset)
-                else:
-                    velo=self.optimisedtrain(bdataset, velo)
-            velocity=velo
+        # now to test
+        newprediction=self.forward(inputs)
+        newloss=self.lossfn.getloss(newprediction, expectation)
+        print(newloss)
 
 
 
+# basic testing, we test with a linear function 
 
+#init dataset
 
-                
-  
-
-
-
-            
-
-                
-            
-
-
-
-
-
-
-
-
-                    
-
-                
-
-
-
-
-# test if feedforward of neuron is working
-'''
-net_inputs=[1]
-layerdims=[3,5,1]
-net1=net(3, layerdims)
-net1.forward(net_inputs)
-print(net1.outputs)
-print(net1.weights)
-print(net1.biases)
-'''
-
-
-
-
-
-# create a dataset
-size=100
+numitems=20
 dataset=[]
+def algo(x):
+    y=2*x
+    return y 
+for i in range(numitems):
+    x=rand.randrange(-10,10)
+    y=algo(x)
+    dataset.append([[x],y])
 
-# y=2x
 
-for i in range(size):
-    x=[rand.randrange(-100,100)]
-    y=[2*x[0]]
-    dataset.append([x,y])
-#print(dataset)
 
-layerdims=[3,3,1]
-lr=0.1
-loss=MSE_loss()
-activation=ReLU()
-net1=net(1, layerdims,activation,loss , lr)
-batch=60
-v=net1.optimisedtrain(dataset[:batch])
+repeats=10
+act1=Linear()
+loss1=MSE()
+numinputs=1 
+lr=0.01
 
-net1.optvartrain(dataset, 20, 1000)
+n1=neuron(numinputs, act1, loss1, lr)
+
+print(n1.forward(dataset[3][0]))
+
+for i in range(repeats):
+    print("Repeat:", i)
+    for data in dataset:
+        n1.train(data)
+
+print(n1.forward(dataset[3][0]),dataset[3][1])
+
+
+
+
+
+
